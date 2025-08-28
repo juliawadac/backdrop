7301c25a411ac432e387fead8640ce8f4de5abbc
ArthurDezan <apdezan78@gmail>
2025-08-28 10:37:44 -0300

    controllers, routes, config do banco, server

--- config/db.js
+++ config/db.js
@@ -1,18 +1,16 @@
 import mysql from "mysql2";
 
-// cria a conexão com o banco
 const connection = mysql.createConnection({
-  host: "localhost", 
-  port: 3007,
+  host: "localhost",
+  port: 3307,
   user: "root",
   password: "root",
   database: "drop"
 });
 
-// abre a conexão
 connection.connect(err => {
   if (err) {
-    console.error("Erro ao conectar no banco:", err);
+    console.error("❌ Erro ao conectar no banco:", err);
     return;
   }
   console.log("✅ Conectado ao MySQL!");
